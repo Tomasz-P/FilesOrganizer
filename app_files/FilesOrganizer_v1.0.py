5a093c9313c1cc89bf0c0073b3dcaea11fef4536
Tomasz Pietka <33056585+Tomasz-P@users.noreply.github.com>
2019-11-24 19:19:51 +0100

    Starting the project

---
+++ app_files/FilesOrganizer_v1.0.py
@@ -0,0 +1,15 @@
+'''
+A script enabling files organization in various indicated directories.
+Version: 1.0
+Author: Tomasz Piętka (tomaszpwlkp@o2.pl)
+'''
+
+# DEFINED FUNCTIONS
+def main():
+    '''Main function of the script.'''
+    pass
+
+
+# MAIN PROGRAM
+if __name__ == "__main__":
+    main()
---
+++ app_files/files_organization.py
@@ -0,0 +1,14 @@
+'''
+A module with a class to organize files in directories
+Version: 1.0
+Author: Tomasz Piętka (tomaszpwlkp@o2.pl)
+'''
+
+# DEFINED CLASSES
+
+class FilesOrganization(object):
+    '''Organizing files stored in one main directory in various indicated directories.'''
+
+    def __init__(self):
+        '''Class constructor.'''
+        pass
