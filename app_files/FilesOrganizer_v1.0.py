97f025b2de05878e7339f6708b984e14d6ec5ed6
Tomasz Pietka <33056585+tomasz-p@users.noreply.github.com>
2019-12-01 19:22:16 +0100

    Function 'load_settings()' added.

--- app_files/FilesOrganizer_v1.0.py
+++ app_files/FilesOrganizer_v1.0.py
@@ -4,12 +4,30 @@ Version: 1.0
 Author: Tomasz Piętka (tomaszpwlkp@o2.pl)
 '''
 
+import configparser
+
+
 # DEFINED FUNCTIONS
 def main():
     '''Main function of the script.'''
-    pass
+    settings = load_settings('FilesOrganizer_cfg.ini')
+    print(settings)
+
+
+def load_settings(filename):
+    '''Load settings from configuration file.'''
+    settings = {}
+    config = configparser.ConfigParser()
+    config.read(filename)
+    for section in config.sections():
+        options_dictionary = {}
+        for option in config.options(section):
+            options_dictionary[option] = config[section][option]
+        settings[section] = options_dictionary
+    return settings
 
 
 # MAIN PROGRAM
+
 if __name__ == "__main__":
     main()
