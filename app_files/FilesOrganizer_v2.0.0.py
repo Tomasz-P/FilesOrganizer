5ca19b8400346def9ce13afcf5df154a181b4534
Tomasz <33056585+Tomasz-P@users.noreply.github.com>
2019-12-28 21:43:24 +0100

    Version 2.0.0 finished.

--- app_files/FilesOrganizer_v2.0.0.py
+++ app_files/FilesOrganizer_v2.0.0.py
@@ -39,20 +39,22 @@ def rename_files_and_move(object, files_list, source_path, destination_path):
         if returned_filename == new_full_filename:
             # Executed if the file is not empty - returned unchanged 'new_full_filename'.
             correct_files.append(returned_filename)
-            object.move_file(new_full_filename, source_path, destination_path)            # TODO - SHOULD BE UNHASHED
-            logging.info(f'File {new_full_filename} was moved from {source_path} to {destination_path}.')
+            object.move_file(new_full_filename, source_path, destination_path)
+            print(f'File {new_full_filename} moved from {source_path} to {destination_path}')
+            logging.info(f'File {new_full_filename} moved from {source_path} to {destination_path}.')
         else:
             # Executed if the file is empty - returned changed 'labelled fullfilename'.
             empty_files.append(returned_filename)
+            print(f'File {new_full_filename} is empty. Not moved.')
             logging.warning(f'File {new_full_filename} is empty. It was not moved.')
     return correct_files, empty_files
 
 def main():
     """Main function of the script."""
     try:
-        config_file = 'FilesOrganizer_cfg.ini'
+        CONFIG_FILE = 'FilesOrganizer_cfg.ini'
         # Settings used by the script code.
-        SETTINGS = load_settings(config_file)
+        SETTINGS = load_settings(CONFIG_FILE)
         source_folder = SETTINGS['general_settings']['source_folder']
         file_prefixes_to_dirs_name = SETTINGS['filenames_startswith']
         log_filename = SETTINGS['general_settings']['logfilename']
@@ -67,7 +69,7 @@ def main():
         logger.archive_logfile(log_filename, 5242880)
         logger.set_logging_params(log_filename, log_level)
         logging.info('The program started.')
-        logging.info(f'Settings loaded from file "{config_file}". Parameters in dictionary format: {SETTINGS}.')
+        logging.info(f'Settings loaded from file "{CONFIG_FILE}". Parameters in dictionary format: {SETTINGS}.')
         organizer = FilesOrganization(logging_status='ON')
         devs_without_files = []
         valid_files = []
@@ -77,27 +79,31 @@ def main():
             destination_folder = file_prefixes_to_dirs_name[file_prefix]
             if files_list == []:
                 devs_without_files.append(destination_folder.split('/')[-1])
-                logging.info(f'No files with prefix {file_prefix} were found.')
+                print(f'No files wih prefix {file_prefix} found')
+                logging.info(f'No files with prefix {file_prefix} found.')
             else:
                 cf, ef = rename_files_and_move(organizer, files_list, source_folder, destination_folder)
                 valid_files += cf
                 empty_files += ef
-                logging.info(f'Files with prefix "{file_prefix}" were isolated: {files_list}.')
-                logging.debug(f'Destination folder for prefix {file_prefix} was specified as {destination_folder}.')
+                logging.info(f'Files with prefix "{file_prefix}" isolated: {files_list}.')
+                logging.debug(f'Destination folder for prefix {file_prefix} specified as {destination_folder}.')
         body = MailBodies()
         email_body = body.create_files_organizing_email_body(valid_files, empty_files, devs_without_files)
-        print(email_body)
-        #notify = EmailNotifications()
-        #notify.send_notification(smtp_server, smtp_server_port, email_sender, email_receivers, email_subject, email_body)
-        logging.info(f'A notification email was sent to {email_receivers}.')
+        notify = EmailNotifications()
+        notify.send_notification(smtp_server, smtp_server_port, email_sender, email_receivers, email_subject, email_body)
+        logging.info(f'A notification email sent to {email_receivers}.')
         logging.info('The program finished.')
     except FileNotFoundError as fnfe:
-        print('\nConfig file was not found!\nDetails in logfile.', fnfe)
-        logging.error('Działanie programu zakończone wystąpieniem wyjątku: ', exc_info=True)
+        print('\nConfig file not found!\nDetails in logfile.', fnfe)
+        logging.error('The program finished with exception: ', exc_info=True)
     except configparser.Error as cpe:
-        print('\nAn exception associated with the configuration file has occurred.\nCheck the configuration file. Details in logfile.', cpe)
+        print('\nAn exception associated with the configuration file has occurred.\n'
+              'Check the configuration file. Details in logfile.', cpe)
         logging.error(f'An error with configuration file occurred:', exc_info=True)
-
+    except:
+        print('\nError occurred during program execution!.'
+              '\nCheck the logfile for details.')
+        logging.error(f'An error occurred during program execution: ', exc_info=True)
 
 # MAIN PROGRAM
 
